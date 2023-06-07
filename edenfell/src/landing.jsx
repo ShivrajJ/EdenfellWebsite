cff7b7b6e49776a53dd0ff7235ebe7c93d172042
ShivrajJ <shivraj.jodha@outlook.com>
2023-06-08 02:51:24 +0530

    Minor Layout Changes for User Friendliness

--- edenfell/src/landing.jsx
+++ edenfell/src/landing.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 export default function Landing() {
     return (
         <div className="landing-div">
-            {/* <img alt="" className='landing-img' src={require('./images/landing-image.png')} /> */}
+            <div className="landing-text-div">
+                <div className="landing-flex">
+                    <div className="landing-small-top-text">
+                        <p>SOW THE WIND</p>
+                    </div>
+                    <div className='landing-big-div'>
+                        <h1 className="landing-big-text">EDENFELL</h1>
+                        <h2 className="landing-big-subtext">The Four Cities</h2>
+                    </div>
+                    <div className="landing-small-bottom-text">
+                        <p>REAP THE WHIRLWIND</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
