61d2f9fe4b8444a0f6c19b05cdf7962f8ff52903
ShivrajJ <shivraj.jodha@outlook.com>
2023-06-08 22:07:12 +0530

    Lore Page and Favicon

--- edenfell/vite.config.js
+++ edenfell/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   base: "/EdenfellWebsite/",
-  plugins: [react()]
+  plugins: [svgr(), react()]
 });
