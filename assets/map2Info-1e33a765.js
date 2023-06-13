1ad94f276320742e36ab2c7b58a17a6b62c9bc66
ShivrajJ <shivraj.jodha@outlook.com>
2023-06-13 14:03:04 +0530

    Updates

---
+++ assets/map2Info-1e33a765.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-3b881a6f.js";function n(){return o.jsx("h2",{children:"Info about This Location!"})}export{n as default};
--- assets/map2Info-be049c7f.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-1d71c370.js";function n(){return o.jsx("h2",{children:"Info about This Location!"})}export{n as default};
